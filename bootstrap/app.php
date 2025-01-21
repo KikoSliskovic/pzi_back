d8729ea0b045662198cc77461371caf0e0c2ae7a
KikoSliskovic <kristijan.sliskovic@fpmoz.sum.ba>
2025-01-21 16:53:35 +0100

    Promjenjen back 21.1.2025

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: 'api',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
-    })
+        // $middleware->statefulApi();
+        })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+    
